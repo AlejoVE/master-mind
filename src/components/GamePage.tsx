bfe7dab323d796ba6d0af953cfbba38be58785e3
Alina Marasca <75176258+alinamarasca@users.noreply.github.com>
2022-11-10 01:06:16 +0100

    start page and css

--- src/components/GamePage.tsx
+++ src/components/GamePage.tsx
@@ -1,29 +1,31 @@
-import AvailableColors from './AvailableColors';
-import Row from './Row';
-import ShowCode from './ShowCode';
-import { useContext, useEffect } from 'react';
-import { GameContext } from '../context/gameContext';
+import AvailableColors from "./AvailableColors";
+import Row from "./Row";
+import ShowCode from "./ShowCode";
+import { useContext, useEffect } from "react";
+import { GameContext } from "../context/gameContext";
+import { Link } from "react-router-dom";
 
 function GamePage() {
-	const { score, gameRows, setAvailableRows } = useContext(GameContext);
-	const gameRowsArr = Array.from(Array(gameRows).keys());
+  const { score, gameRows, setAvailableRows } = useContext(GameContext);
+  const gameRowsArr = Array.from(Array(gameRows).keys());
 
-	useEffect(() => {
-		setAvailableRows(gameRows);
-	}, [gameRows, setAvailableRows]);
+  useEffect(() => {
+    setAvailableRows(gameRows);
+  }, [gameRows, setAvailableRows]);
 
-	return (
-		<>
-			<h2>SCORE: {score}</h2>
-			<div className='page-content'>
-				<ShowCode />
-				{gameRowsArr.map((row, i) => (
-					<Row key={i} />
-				))}
-				<AvailableColors />
-			</div>
-		</>
-	);
+  return (
+    <>
+      <Link to={"/start"}>Home</Link>
+      <h2>SCORE: {score}</h2>
+      <div className="page-content">
+        <ShowCode />
+        {gameRowsArr.map((row, i) => (
+          <Row key={i} />
+        ))}
+        <AvailableColors />
+      </div>
+    </>
+  );
 }
 
 export default GamePage;
