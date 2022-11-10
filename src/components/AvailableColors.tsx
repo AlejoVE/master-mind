e4b4810d9a2c2762f174c8feb8584ede1a566c9e
Alejandro Montilla <amontilla@luckycycle.com>
2022-11-10 14:28:25 +0100

    Fix bug AvailableColors

--- src/components/AvailableColors.tsx
+++ src/components/AvailableColors.tsx
@@ -1,41 +1,44 @@
-import React, { useContext } from "react";
-import Circle from "./Circle";
-import { GameContext } from "../context/gameContext";
-import { Colors } from "../types/types";
+import React, { useContext } from 'react';
+import Circle from './Circle';
+import { GameContext } from '../context/gameContext';
+import { Colors } from '../types/types';
 
 function AvailableColors() {
-  {
-    /* indication which color is selected */
-  }
-  const { setCurrentColor, currentColor } = useContext(GameContext);
-  const colors = Object.values(Colors);
-  const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
-    const color: string = (e.target as HTMLButtonElement).getAttribute(
-      "data-color"
-    ) as string;
-    setCurrentColor(color);
-  };
+	/* indication which color is selected */
+	const { setCurrentColor, currentColor } = useContext(GameContext);
+	const colors = Object.values(Colors);
+	const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+		const color: string = (e.target as HTMLButtonElement).getAttribute(
+			'data-color'
+		) as string;
+		setCurrentColor(color);
+	};
 
-  return (
-    <>
-      {/* indication which color is selected */}
-      <Circle
-        disabled={true}
-        onClick={handleClick}
-        color={currentColor}
-        size="4"
-      />
-      <div className="available-colors-container">
-        {colors.map(color => {
-          return (
-            <div className="square">
-              <Circle onClick={handleClick} color={color} size="4" />
-            </div>
-          );
-        })}
-      </div>
-    </>
-  );
+	return (
+		<>
+			{/* indication which color is selected */}
+			<Circle
+				disabled={true}
+				onClick={handleClick}
+				color={currentColor}
+				size='4'
+			/>
+			<div className='available-colors-container'>
+				{colors.map((color, i) => {
+					return (
+						<div key={i} className='square'>
+							<Circle
+								key={i}
+								onClick={handleClick}
+								color={color}
+								size='4'
+							/>
+						</div>
+					);
+				})}
+			</div>
+		</>
+	);
 }
 
 export default AvailableColors;
