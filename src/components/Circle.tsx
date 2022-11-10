68a7a372c0b61b0cb89b1f29ebf482b9bc951750
Alejandro Montilla <amontilla@luckycycle.com>
2022-11-10 12:20:11 +0100

    Revert changes in Circle component

--- src/components/Circle.tsx
+++ src/components/Circle.tsx
@@ -8,43 +8,19 @@ type ButtonProps = {
 
 function Circle(props: ButtonProps) {
 	return (
-		<>
-			{props.gameOver ? (
-				<button
-					disabled={props.disabled}
-					className='button'
-					onClick={props.onClick}
-					data-color={props.color}
-					style={{
-						backgroundColor: `${`${props.color || '#53483F'}`}`,
-						width: `${props.size}rem`,
-						minWidth: `${props.size}rem`,
-						height: `${props.size}rem`,
-						minHeight: `${props.size}rem`,
-					}}
-				>
-					GAME OVER
-				</button>
-			) : (
-				<button
-					disabled={props.disabled}
-					className='button'
-					onClick={props.onClick}
-					data-color={props.color}
-					style={{
-						backgroundColor: `${
-							props.gameOver ? '#53483F' : `${props.color || '#53483F'}`
-						}`,
-						width: `${props.size}rem`,
-						minWidth: `${props.size}rem`,
-						height: `${props.size}rem`,
-						minHeight: `${props.size}rem`,
-					}}
-				>
-					PLAYING
-				</button>
-			)}
-		</>
+		<button
+			disabled={props.disabled}
+			className='button'
+			onClick={props.onClick}
+			data-color={props.color}
+			style={{
+				backgroundColor: `${`${props.color || '#53483F'}`}`,
+				width: `${props.size}rem`,
+				minWidth: `${props.size}rem`,
+				height: `${props.size}rem`,
+				minHeight: `${props.size}rem`,
+			}}
+		></button>
 	);
 }
 
